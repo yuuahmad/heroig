46cb5aef1856df91b9f5196367a6a408b990e5f0
yusuf <ahmadyusufmaulana0@gmai.com>
2021-02-26 10:37:33 +0700

    komit saya pada auth dan login

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,10 @@
 // ini adalah kode main pada aplikasi heroig
 // kode ini digunakan untuk melakukan routing atau pindah2 halaman
 // koe ini adalah kode utama yang memuat semua anak halaman
-import 'dart:async';
-import 'dart:io' show Platform;
-
-import 'package:firebase_core/firebase_core.dart';
-
 import 'package:flutter/material.dart';
 import 'package:heroig/HalamanUtama.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final FirebaseApp app = await Firebase.initializeApp(
-    name: 'db2',
-    options: Platform.isIOS || Platform.isMacOS
-        ? FirebaseOptions(
-            appId:
-                '1:497685752739:ios:2ffb356f1c26a030428816', //ini sudah saya ganti
-            apiKey: 'AIzaSyBhmPyJxIr7NPbYFwFlsZ1pPr33c8jkwBg', //ganti
-            projectId: 'heroigitera', //ini sudah saya ganti
-            messagingSenderId: '497685752739', //ini sudah saya ganti
-            databaseURL:
-                'https://heroigitera-default-rtdb.firebaseio.com/', //ini sudah saya ganti
-          )
-        : FirebaseOptions(
-            appId:
-                '1:497685752739:android:93ab3a7bef37b2ed428816', //ini sudah saya ganti
-            apiKey: 'AIzaSyBhmPyJxIr7NPbYFwFlsZ1pPr33c8jkwBg', //ganti
-            messagingSenderId: '497685752739', //ini sudah saya ganti
-            projectId: 'heroigitera', //ini sudah saya ganti
-            databaseURL:
-                'https://heroigitera-default-rtdb.firebaseio.com/', //ini sudah saya ganti
-          ),
-  );
+main(List<String> args) {
   runApp(AplikasiSaya());
 }
 
