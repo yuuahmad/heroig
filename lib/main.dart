20993e08206a3dddd01e6898c5b244b8b9fcd4dd
yusuf <ahmadyusufmaulana0@gmai.com>
2021-02-28 19:14:15 +0700

    ini bagian ulang dari awal

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:heroig/pages/wrapper.dart';
+import 'package:heroig/pages/main_page.dart';
 
-// ini adalah main app dari aplikasi heroig itera
-// tujuannya adalah untuk melakukan main function
-// aplikasi hanya akan bisa diplay disini.
-
-Future main(List<String> args) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    // pergi ke halaman wrapper untuk menentukan apakah user sudah login atau belum
-    return MaterialApp(theme: ThemeData.dark(), home: Wrapper());
+    return MaterialApp(title: "Heroig App", home: Home());
   }
 }
